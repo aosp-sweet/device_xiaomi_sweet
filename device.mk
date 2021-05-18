b1f04c1ad385a7c274f035a27f51f9a0440d947a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-18 20:01:12 +0530

    sweet: Add ESE/secure_element support for NFC
    - seems to be required for nfc payments
    
    Change-Id: Ic6c03d61a4621ee4b6f5e5af27eb569b9cfa4fa0

--- device.mk
+++ device.mk
@@ -218,6 +218,9 @@ PRODUCT_PACKAGES += \
 # NFC
 TARGET_NFC_SKU := nfc
 
+PRODUCT_PACKAGES += \
+    SecureElement
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
