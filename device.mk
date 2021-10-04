12ddc88e49a4eff8c3574bdf5e4a3f51cf4faf3e
raghavt20 <raghavtandon2000@gmail.com>
2021-10-04 17:09:31 +0530

    sweet: Import IRSC security configurations

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.target.rc \
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/input/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
